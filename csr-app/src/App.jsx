bfd1abff8f65e6feb31352f927d2bf7f6fe7ba28
DaniAkash <dani@oslash.com>
2023-01-28 06:23:33 +0530

    feat: setup tailwind

--- csr-app/src/App.jsx
+++ csr-app/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { tw } from 'twind'
 import { Link, Route } from "wouter";
 
 const AllPosts = () => {
@@ -14,18 +13,18 @@ const AllPosts = () => {
   }, [])
 
   return (
-    <div className={tw`bg-white`}>
-      <div className={tw`mx-auto max-w-7xl divide-y divide-gray-900/10 px-6 py-24 sm:py-32 lg:py-40 lg:px-8`}>
-        <h2 className={tw`text-2xl font-bold leading-10 tracking-tight text-gray-900`}>Client side rendered page</h2>
-        <dl className={tw`mt-10 space-y-8 divide-y divide-gray-900/10`}>
+    <div className={"bg-white"}>
+      <div className={"mx-auto max-w-7xl divide-y divide-gray-900/10 px-6 py-24 sm:py-32 lg:py-40 lg:px-8"}>
+        <h2 className={"text-2xl font-bold leading-10 tracking-tight text-gray-900"}>Client side rendered page</h2>
+        <dl className={"mt-10 space-y-8 divide-y divide-gray-900/10"}>
           {data.map((each, index) => (
-            <div key={index.toString()} className={tw`pt-8 lg:grid lg:grid-cols-12 lg:gap-8`}>
-              <dt className={tw`text-base font-semibold leading-7 text-gray-900 lg:col-span-5`}>{`Post - ${(index).toString()}`}</dt>
-              <dd className={tw`mt-4 lg:col-span-7 lg:mt-0`}>
-                <p className={tw`text-base leading-7 text-gray-600`}>{each}</p>
+            <div key={index.toString()} className={"pt-8 lg:grid lg:grid-cols-12 lg:gap-8"}>
+              <dt className={"text-base font-semibold leading-7 text-gray-900 lg:col-span-5"}>{`Post - ${(index).toString()}`}</dt>
+              <dd className={"mt-4 lg:col-span-7 lg:mt-0"}>
+                <p className={"text-base leading-7 text-gray-600"}>{each}</p>
                 <Link href={`/${index}`}>
                   {/* rome-ignore lint/a11y/useValidAnchor: link used from wouter */}
-                  <a className={tw`text-base font-semibold text-blue-600 hover:text-blue-900`}>Read More</a>
+                  <a className={"text-base font-semibold text-blue-600 hover:text-blue-900"}>Read More</a>
                 </Link>
               </dd>
             </div>
@@ -47,10 +46,10 @@ const IndividualPost = ({ id }) => {
   }, [])
 
   return (
-    <div className={tw`pt-8 lg:grid lg:grid-cols-12 lg:gap-8`}>
-      <dt className={tw`text-base font-semibold leading-7 text-gray-900 lg:col-span-5`}>{`Post - ${(parseInt(id)).toString()}`}</dt>
-      <dd className={tw`mt-4 lg:col-span-7 lg:mt-0`}>
-        <p className={tw`text-base leading-7 text-gray-600`}>{data?.data ?? ""}</p>
+    <div className={"pt-8 lg:grid lg:grid-cols-12 lg:gap-8"}>
+      <dt className={"text-base font-semibold leading-7 text-gray-900 lg:col-span-5"}>{`Post - ${(parseInt(id)).toString()}`}</dt>
+      <dd className={"mt-4 lg:col-span-7 lg:mt-0"}>
+        <p className={"text-base leading-7 text-gray-600"}>{data?.data ?? ""}</p>
       </dd>
     </div>
   )
