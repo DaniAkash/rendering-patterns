cd5929792aa90c5e17c12004fce58d75e66d1968
DaniAkash <dani@oslash.com>
2023-01-28 12:16:39 +0530

    feat: setup rendering patterns for pages

--- edge-ssr-app/src/app/layout.jsx
+++ edge-ssr-app/src/app/layout.jsx
@@ -7,7 +7,9 @@ export default function RootLayout({ children }) {
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <title>Edge SSR App</title>
+      </head>
       <body>{children}</body>
     </html>
   )
